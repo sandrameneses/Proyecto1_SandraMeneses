c07f06a22eeea66bf9e6a4db56154cb84d91c5ba
sandrameneses <sromen@gmail.com>
2023-12-28 10:58:22 -0500

    MAIN.PY

---
+++ main.py
@@ -0,0 +1,27 @@
+
+# le doy la primera funcion para API (PlayTimeGenre)
+
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import HTMLResponse
+import pandas as pd
+import numpy as np
+
+
+df_PlayTimeGenre = pd.read_csv("/user/Documents/SANDRA 2023/HENRY/PROYECTO 1 RESPALDO/DATA FUNCIONES/PlayTimeGenre.csv")
+
+app = FastAPI(title='PI1 - Sandra Meneses')
+
+@app.get("Play_time_genre")
+
+def play_time_genre(x: str):
+
+    # Filtrar el DataFrame para el género específico
+    df_genero = df_PlayTimeGenre.loc[df_PlayTimeGenre['Genero'] == x]
+
+    # Encontrar el año con más horas jugadas para ese género
+    año_max_playtime = df_genero.loc[df_genero['playtime_forever'].idxmax(), 'Año_Lanzamiento']
+
+    # Crear la leyenda
+    leyenda = f"Año de lanzamiento con más horas jugadas para Género {x}: {año_max_playtime}"
+
+    return leyenda
