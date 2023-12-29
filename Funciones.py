7e5f0cd414d702fea1e1651f248ea083cd07025f
sandrameneses <sromen@gmail.com>
2023-12-28 19:51:17 -0500

    OTRAS FUNCIONES

--- Funciones.py
+++ Funciones.py
@@ -19,3 +19,104 @@ def play_time_genre(x: str):
     leyenda = f"Año de lanzamiento con más horas jugadas para Género {x}: {año_max_playtime}"
 
     return leyenda
+
+
+# Segunda funcion UserForGenre
+
+def user_for_genre(x: str):
+
+    # Leer el DataFrame desde el archivo CSV
+    dataframe = pd.read_csv("DATA FUNCIONES/UserForGenre.csv")
+
+    # Filtrar el DataFrame por el género dado
+    genre_df = dataframe.loc[dataframe['Genero'] == x]
+
+    if genre_df.empty:
+        return {"error": "No hay datos para el género proporcionado"}
+
+    # Encontrar el usuario con más horas jugadas
+    max_hours_user = genre_df.loc[genre_df['Horas_Jugadas'].idxmax()]['Id_user']
+
+    # Agrupar por año y sumar las horas jugadas
+    hours_by_year = genre_df.groupby('Año_Lanzamiento')['Horas_Jugadas'].sum().reset_index()
+
+    # Convertir el resultado a un formato de lista de diccionarios
+    hours_list = [{'Año': int(row['Año_Lanzamiento']), 'Horas': int(row['Horas_Jugadas'])} for index, row in hours_by_year.iterrows()]
+
+    # Crear el resultado final en el formato deseado
+    result = {"Usuario con más horas jugadas para Género {}".format(x): max_hours_user, "Horas jugadas": hours_list}
+
+    return result
+
+
+# Tercera Funcion Users_Recommend
+
+def users_recommend(x: int):
+    # Leer el DataFrame desde el archivo CSV
+    dataframe = pd.read_csv("DATA FUNCIONES/UsersRecommend.csv")
+    
+    # Filtrar el DataFrame por el año dado
+    year_df = dataframe.loc[dataframe['Año'] == x ]
+
+    if year_df.empty:
+        return {"error": "No hay datos para el año proporcionado"}
+
+    # Ordenar el DataFrame por la cantidad de recomendaciones en orden descendente
+    sorted_df = year_df.sort_values(by='Recomendaciones', ascending=False)
+
+    # Tomar los tres juegos más recomendados
+    top3_games = sorted_df.head(3)['Juego'].tolist()
+
+    # Convertir el resultado a un formato de lista de diccionarios con los puestos correctos
+    result = [{"Puesto {}".format(i + 1): juego} for i, juego in enumerate(top3_games)]
+
+    return result
+
+
+# Cuarta funcion UsersNotRecommend
+
+def users_not_recommend(x: int):
+    # Leer el DataFrame desde el archivo CSV
+    dataframe = pd.read_csv("DATA FUNCIONES/UsersNotRecommend.csv")
+
+    # Filtrar el DataFrame por el año dado
+    year_df = dataframe.loc[dataframe['Año'] == x]
+
+    if year_df.empty:
+        return {"error": "No hay datos para el año proporcionado"}
+
+    # Ordenar el DataFrame por la cantidad de recomendaciones en orden ascendente
+    sorted_df = year_df.sort_values(by='Recomendaciones')
+
+    # Tomar los tres juegos menos recomendados
+    bottom3_games = sorted_df.head(3)['Juego'].tolist()
+
+    # Convertir el resultado a un formato de lista de diccionarios con los puestos correctos
+    result = [{"Puesto {}".format(i + 1): juego} for i, juego in enumerate(bottom3_games)]
+
+    return result
+
+# Quinta Funcion Sentiment_Analysis
+
+def sentiment_analysis(x: int):
+    # Leer el DataFrame desde el archivo CSV
+    dataframe = pd.read_csv("DATA FUNCIONES/Sentiment_Analysis.csv")
+
+    # Filtrar el DataFrame por el año dado
+    year_df = dataframe.loc[dataframe['Año_Lanzamiento'] == x]
+
+    if year_df.empty:
+        return {"error": "No hay datos para el año proporcionado"}
+
+    # Obtener la cantidad de registros para cada categoría de análisis de sentimiento
+    negativo = year_df[year_df['sentiment_analysis'] == 0]['Registros'].sum()
+    neutral = year_df[year_df['sentiment_analysis'] == 1]['Registros'].sum()
+    positivo = year_df[year_df['sentiment_analysis'] == 2]['Registros'].sum()
+
+    # Crear el resultado en el formato deseado
+    resultado = {"Negativo": negativo,
+              "Neutral": neutral,
+              "Positiv0": positivo}
+
+    return resultado
+
--- main.py
+++ main.py
@@ -11,8 +11,42 @@ importlib.reload(FU)
 
 app = FastAPI(title='PI1 - Sandra Meneses')
 
+# 1
+
 @app.get(path = "/play_time_genre")
 
 def play_time_genre(x: str):
 
     return FU.play_time_genre(x)
+
+# 2
+
+@app.get(path = "/user_for_genre")
+
+def user_for_genre(x: str):
+
+    return FU.user_for_genre(x)
+
+# 3 
+
+@app.get(path = "/users_recommend")
+
+def users_recommend(x: int):
+
+    return FU.users_recommend(x)
+
+# 4
+
+@app.get(path = "/users_no_recommend")
+
+def users_no_recommend(x: int):
+
+    return FU.users_recommend(x)
+
+# 5
+
+@app.get(path = "/sentiment_analysis")
+
+def sentiment_analysis(x: int):
+
+    return FU.sentiment_analysis(x)
